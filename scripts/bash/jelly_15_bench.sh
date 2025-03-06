3a48983993eb4e6e93f0e62c5cf86d0f455166bc
key-r-code <kramjattun@gmail.com>
2025-03-06 17:43:52 -0500

    updated script

--- scripts/bash/jelly_15_bench.sh
+++ scripts/bash/jelly_15_bench.sh
@@ -1,5 +1,5 @@
 main_dir="/ifs/groups/rosenMRIGrp/kr3288/extended/benchmarks"
-splits=("groupaa" "groupab" "groupac" "groupad" "groupae" "groupaf" "groupag" "groupah" "groupai" "groupaj")
+splits=("groupaa" "groupab" "groupac" "groupad" "groupae" "groupaf" "groupag" "groupah" "groupai" "groupaj" "groupak" "groupal" "groupam")
 for split in "${splits[@]}"; do
 	python3 script_generatorv2.py jelly-15-mers_${split}.sh "/ifs/groups/rosenMRIGrp/kr3288/jellyfish_gen_new_UPDATED.bash /ifs/groups/rosenMRIGrp/kr3288/extended/benchmarks/${split} 15 false" /ifs/groups/rosenMRIGrp/kr3288/bench_jelly
 done
